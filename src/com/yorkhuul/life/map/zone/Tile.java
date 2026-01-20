ccd5d28897a0a48b2adb86db80137c279ca9a2d8
Ludvinae <benoit.champlon79@gmail.com>
2026-01-20 19:04:41 +0100

    feat(Zone): made Tile class with altitude attribute and methods to alter it

--- src/com/yorkhuul/life/map/zone/Tile.java
+++ src/com/yorkhuul/life/map/zone/Tile.java
@@ -1,4 +1,45 @@
 package com.yorkhuul.life.map.zone;
 
 public class Tile {
+
+    private float altitude;
+
+    public Tile(float altitude) {
+        setAltitude(altitude);
+    }
+
+    // Getters
+    public float getAltitude() {
+        return altitude;
+    }
+
+    // Setters
+    public void setAltitude(float altitude) {
+        this.altitude = this.clamp(altitude);
+    }
+
+    // Others
+    @Override
+    public String toString() {
+        return String.valueOf(getAltitude());
+    }
+
+    // Methods
+    public float clamp(float altitude) {
+        if (altitude > 1) {
+            return 1;
+        }
+        if (altitude < -1) {
+            return -1;
+        }
+        return altitude;
+    }
+
+    public void add(float value) {
+        setAltitude(getAltitude() + value);
+    }
+
+    public void multiply(float factor) {
+        setAltitude(getAltitude() * factor);
+    }
 }
