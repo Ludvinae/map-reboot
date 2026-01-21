78ba54eff1e5af26728b1980ad476547448748c5
unknown <benoit.champlon79@gmail.com>
2026-01-21 13:53:02 +0100

    feat(shape): added rectangle shape class with methods to calcul influence, still work to do on it

---
+++ src/com/yorkhuul/life/map/terraforming/RectangleShape.java
@@ -0,0 +1,57 @@
+package com.yorkhuul.life.map.terraforming;
+
+import com.yorkhuul.life.map.tools.Coordinates;
+import com.yorkhuul.life.map.tools.Distance;
+
+public class RectangleShape extends Shape{
+    private Coordinates start;
+    private int width;
+    private int height;
+
+    public RectangleShape(float strength, float falloff, Coordinates start, int width, int height) {
+        super(strength, falloff);
+        this.start = start;
+        this.width = width;
+        this.height = height;
+    }
+
+    // Getters
+    // wip
+    @Override
+    public void getEffect() {
+
+    }
+
+    public Coordinates getEnd() {
+        return new Coordinates(start.x() + this.width, start.y() + this.height);
+    }
+
+    public Coordinates getCenter() {
+        Coordinates end = getEnd();
+        int x = Math.floorDiv (end.x() - start.x(), 2);
+        int y = Math.floorDiv (end.y() - start.y(), 2);
+        return new Coordinates(x, y);
+    }
+
+    // Setters
+
+    // Others
+
+    // Methods
+    public double distanceFromCenter(Coordinates coords) {
+        Coordinates center = getCenter();
+        Distance dist = new Distance(coords, center);
+        return dist.euclidianDistance();
+    }
+
+    public double influence(Coordinates coords) {
+        double dist = distanceFromCenter(coords);
+
+        double influence = this.getStrength() - (getFalloff() * dist);
+        if (influence < 0) {
+            influence = 0;
+        }
+        return influence;
+    }
+
+}
