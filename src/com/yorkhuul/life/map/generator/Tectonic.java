23731b0b824dd97b4fd03e3acde47355549af19e
Ludvinae <benoit.champlon79@gmail.com>
2026-01-22 22:19:50 +0100

    feat(generator): tectonic class allow to apply rift or subduction effect to the map, using DivideMapShape

---
+++ src/com/yorkhuul/life/map/generator/Tectonic.java
@@ -0,0 +1,72 @@
+package com.yorkhuul.life.map.generator;
+
+import com.yorkhuul.life.map.effect.AddEffect;
+import com.yorkhuul.life.map.effect.Effect;
+import com.yorkhuul.life.map.effect.Line;
+import com.yorkhuul.life.map.effect.ShapeEffect;
+import com.yorkhuul.life.map.shape.DivideMapShape;
+import com.yorkhuul.life.map.shape.Shape;
+import com.yorkhuul.life.map.tools.Coordinates;
+import com.yorkhuul.life.map.tools.RandomInteger;
+import com.yorkhuul.life.map.tools.RandomSpot;
+import com.yorkhuul.life.map.zone.World;
+
+import java.util.Objects;
+
+public class Tectonic implements GenerationStep {
+
+    private int count;
+    private String type;
+    private int influenceMin;
+    private int influenceMax;
+    private float strength;
+
+    public Tectonic(int count, String type, int minRadius, int maxRadius, float strength) {
+        setCount(count);
+        setType(type);
+        this.influenceMin = minRadius;
+        this.influenceMax = maxRadius;
+        this.strength = strength;
+    }
+
+    // Setters
+    public void setCount(int count) {
+        if (count <= 1) {
+            count = 1;
+        }
+        this.count = count;
+    }
+
+    public void setType(String type) {
+        if (!Objects.equals(type, "rift") && !Objects.equals(type, "subduction")) {
+            type = "subduction";
+        }
+        this.type = type;
+    }
+
+    public void setInfluenceMax(int influenceMax) {
+        if (influenceMax <= this.influenceMin) {
+            influenceMax = this.influenceMin + 1;
+        }
+        this.influenceMax = influenceMax;
+    }
+
+    //Methods
+    @Override
+    public void apply(World world) {
+        for (int i = 0; i < this.count; i++) {
+            Coordinates coordsStart = new RandomSpot(world.getWidthInTiles(), world.getHeightInTiles()).getCoords();
+            Coordinates coordsEnd = new RandomSpot(world.getWidthInTiles(), world.getHeightInTiles()).getCoords();
+            while (coordsStart == coordsEnd) {
+                coordsEnd = new RandomSpot(world.getWidthInTiles(), world.getHeightInTiles()).getCoords();
+            }
+
+            int radius = new RandomInteger(this.influenceMin, this.influenceMax).getRandomInt();
+            Line line = new Line(coordsStart, coordsEnd);
+            Effect effect = new AddEffect();
+            Shape divideMap = new DivideMapShape(line, type, radius, strength);
+
+            ShapeEffect tectonic = new ShapeEffect(divideMap, effect);
+        }
+    }
+}
