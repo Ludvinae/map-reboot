000a3acad2fbebad040392edb5ae3cc961bea9a3
unknown <benoit.champlon79@gmail.com>
2026-01-23 12:02:26 +0100

    chore(main) : modified the parameters for world generation, wip

--- src/com/yorkhuul/life/map/Main.java
+++ src/com/yorkhuul/life/map/Main.java
@@ -1,21 +1,20 @@
 package com.yorkhuul.life.map;
 
 import com.yorkhuul.life.display.WorldRenderer;
-import com.yorkhuul.life.map.generator.GenerationStep;
-import com.yorkhuul.life.map.generator.OceanBorders;
-import com.yorkhuul.life.map.generator.Volcanic;
-import com.yorkhuul.life.map.generator.WorldGenerator;
+import com.yorkhuul.life.map.generator.*;
 import com.yorkhuul.life.map.zone.World;
 
 import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        World gaia = new World("gaia", 2, 2);
+        World gaia = new World("gaia", 10, 10);
         System.out.println(gaia);
 
 
-        List<GenerationStep> steps = List.of(new Volcanic(5, 10, 50, 0.3f),
+        List<GenerationStep> steps = List.of(new Volcanic(100, 10, 50, 0.3f),
+                new Tectonic(5, "rift", 10, 100, 0.2f),
+                new Tectonic(5, "subduction", 10, 100, 0.2f),
                 new OceanBorders(10, 1f));
         WorldGenerator generator = new WorldGenerator(steps);
         generator.generate(gaia);
