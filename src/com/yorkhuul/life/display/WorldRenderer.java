e581a2475de8690ee76d636e37b7ffe80d203113
unknown <benoit.champlon79@gmail.com>
2026-01-23 11:57:17 +0100

    fix(display): fixed inversion of coordinates for retrieving a region in generateImage loop, switched the formula from size * x to size + x

--- src/com/yorkhuul/life/display/WorldRenderer.java
+++ src/com/yorkhuul/life/display/WorldRenderer.java
@@ -5,11 +5,13 @@ import com.yorkhuul.life.map.zone.Tile;
 import com.yorkhuul.life.map.zone.World;
 
 import java.awt.*;
+import java.awt.image.BufferedImage;
 
 public class WorldRenderer {
 
     private World world;
-    private Image image;
+    private BufferedImage image;
+    int regionSize = Region.getSize();
 
     public WorldRenderer(World world) {
         this.world = world;
@@ -19,31 +21,55 @@ public class WorldRenderer {
     public void setImage() {
         int height = world.getHeightInTiles();
         int width = world.getWidthInTiles();
-        this.image = new Image(width, height);
+        System.out.println(height);
+        this.image = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
     }
 
     public void generateImage() {
         for (int i = 0; i < world.getHeight(); i++) {
             for (int j = 0; j < world.getWidth(); j++) {
-                Region region = world.getRegion(i, j);
+                Region region = world.getRegion(j, i);
 
-                for (int y = 0; y < region.getLocalCoordinates().y(); y++) {
-                    for (int x = 0; x < region.getLocalCoordinates().x(); x++) {
+                for (int y = 0; y < regionSize; y++) {
+                    for (int x = 0; x < regionSize; x++) {
+
+                        int worldX = j * regionSize + x;
+                        int worldY = i * regionSize + y;
+
                         Tile tile = region.getTile(x, y);
-                        Color color = interpretMap(tile);
-                        image.setColorToPosition(x, y, color);
+                        Color color = altitudeToColor(tile.getAltitude());
+                        image.setRGB(worldX, worldY, color.getRGB());
                     }
                 }
             }
         }
     }
 
-    public Color interpretMap(Tile tile) {
-        return image.altitudeToColor(tile.getAltitude());
+
+    public Color altitudeToColor(float altitude) {
+        if (altitude < -0.5) {
+            return Color.BLUE;
+        } else if (altitude < 0.0001) {
+            return Color.CYAN;
+        } else if (altitude < 0.05) {
+            return Color.YELLOW;
+        } else if (altitude < 0.35) {
+            return Color.GREEN;
+        } else if (altitude < 0.6) {
+            return Color.BLACK;
+        } else if (altitude <= 1) {
+            return Color.WHITE;
+        } else {
+            // indicates problem in altitude value
+            return Color.RED;
+        }
     }
+
 
     public void exportImage() {
         String name = world.getName() + "_" + System.currentTimeMillis() + ".png";
-        ImageExporter.saveAsPng(image.getImage(), name);
+        ImageExporter.saveAsPng(image, name);
     }
+
+
 }
--- src/com/yorkhuul/life/map/Main.java
+++ src/com/yorkhuul/life/map/Main.java
@@ -11,16 +11,16 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        World gaia = new World("gaia", 10, 10);
+        World gaia = new World("gaia", 2, 2);
         System.out.println(gaia);
 
 
-        List<GenerationStep> steps = List.of(new Volcanic(500, 10, 50, 0.5f),
+        List<GenerationStep> steps = List.of(new Volcanic(5, 10, 50, 0.3f),
                 new OceanBorders(10, 1f));
         WorldGenerator generator = new WorldGenerator(steps);
         generator.generate(gaia);
-        System.out.println(gaia.getRegion(5, 5));
-        System.out.println(gaia.getRegion(5, 5).getTile(5, 5));
+        //System.out.println(gaia.getRegion(5, 5));
+        //System.out.println(gaia.getRegion(5, 5).getTile(5, 5));
         System.out.println(gaia.getRegion(1, 1));
         System.out.println(gaia.getRegion(1, 1).getTile(1, 1));
 
