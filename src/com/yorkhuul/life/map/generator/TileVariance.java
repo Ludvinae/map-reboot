3b381f659f21b46e4bd2cec19f0b293efc57dfd1
Ludvinae <benoit.champlon79@gmail.com>
2026-01-27 18:43:26 +0100

    feat(generator): added a new GenerationStep TileVariance to multiply tile altitude by a number  between 1 - factor and 1 + factor

---
+++ src/com/yorkhuul/life/map/generator/TileVariance.java
@@ -0,0 +1,25 @@
+package com.yorkhuul.life.map.generator;
+
+import com.yorkhuul.life.map.zone.Tile;
+import com.yorkhuul.life.map.zone.World;
+
+public class TileVariance implements GenerationStep {
+
+    private float factor;
+
+    public TileVariance(float factor) {
+        this.factor = factor;
+    }
+
+
+    @Override
+    public void apply(World world) {
+        float lower = 1 - factor;
+        float higher = 1 + factor;
+
+        world.forEachTile((region, localX, localY, worldX, worldY) -> {
+            Tile tile = region.getTile(localX, localY);
+            tile.multiply((float) (lower + (higher - lower * Math.random())));
+        });
+    }
+}
