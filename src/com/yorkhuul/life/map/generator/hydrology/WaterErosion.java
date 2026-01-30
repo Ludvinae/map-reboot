d1a46168c8d8aec30393831215a5324cf5aefe18
unknown <benoit.champlon79@gmail.com>
2026-01-30 13:23:14 +0100

    feat(generator): added a class to apply erosion depending on water flow and slope

--- src/com/yorkhuul/life/map/generator/hydrology/WaterErosion.java
+++ src/com/yorkhuul/life/map/generator/hydrology/WaterErosion.java
@@ -1,7 +1,11 @@
 package com.yorkhuul.life.map.generator.hydrology;
 
 import com.yorkhuul.life.map.generator.GenerationStep;
+import com.yorkhuul.life.map.zone.Tile;
+import com.yorkhuul.life.map.zone.TileWithCoordinates;
 import com.yorkhuul.life.map.zone.World;
+
+import java.util.List;
 
 public class WaterErosion implements GenerationStep {
 
@@ -13,6 +17,18 @@ public class WaterErosion implements GenerationStep {
 
     @Override
     public void apply(World world) {
+        float[][] buffer = new float[world.getHeightInTiles()][world.getWidthInTiles()];
 
+        List<TileWithCoordinates> tiles = world.getTilesContext();
+        for (TileWithCoordinates tile: tiles) {
+            float flow = tile.tile().getFlow();
+            float slope = tile.slope();
+            buffer[tile.worldY()][tile.worldX()] = -(flow * slope * strength);
+
+        }
+        world.forEachTile((region, localX, localY, worldX, worldY) -> {
+            Tile tile = region.getTile(localX, localY);
+            tile.add(buffer[worldY][worldX]);
+        });
     }
 }
