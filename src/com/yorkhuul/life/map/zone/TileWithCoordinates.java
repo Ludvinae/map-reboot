3bf211be50e0b0aa1fd1d3fba4355cb10e9557e9
Ludvinae <benoit.champlon79@gmail.com>
2026-01-31 10:52:11 +0100

    refactor(zone): replaced old TileWithCoordinates record with a class on its own, so it can be mutable to store the flow of each Tile and change depending on the water and neighbors in a given iteration

--- src/com/yorkhuul/life/map/zone/TileWithCoordinates.java
+++ src/com/yorkhuul/life/map/zone/TileWithCoordinates.java
@@ -1,13 +1,27 @@
 package com.yorkhuul.life.map.zone;
 
-public record TileWithCoordinates(
-        Tile tile,
-        int worldX,
-        int worldY,
+public class TileWithCoordinates {
 
-        TileWithCoordinates lowestNeighbor,
-        float slope
-) {
+    private Tile tile;
+    private int worldX;
+    private int worldY;
+
+    private TileWithCoordinates lowestNeighbor;
+    private float slope;
+    private float flow;
+
+    public TileWithCoordinates(Tile tile, int worldX, int worldY, TileWithCoordinates lowestNeighbor, float slope, float flow) {
+        this.tile = tile;
+        this.worldX = worldX;
+        this.worldY = worldY;
+        this.lowestNeighbor = lowestNeighbor;
+        this.slope = slope;
+        this.flow = flow;
+    }
+
+    public TileWithCoordinates(Tile tile, int worldX, int worldY) {
+        this(tile, worldX, worldY, null, 0, 0);
+    }
 
     public float getAltitude() {
         return tile.getAltitude();
@@ -17,4 +31,47 @@ public record TileWithCoordinates(
         return tile.getWater();
     }
 
+    public TileWithCoordinates getLowestNeighbor() {
+        return lowestNeighbor;
+    }
+
+    public float getFlow() {
+        return this.flow;
+    }
+
+    public float getSlope() {
+        return slope;
+    }
+
+    public Tile getTile() {
+        return tile;
+    }
+
+    public int getWorldX() {
+        return worldX;
+    }
+
+    public int getWorldY() {
+        return worldY;
+    }
+
+    public void setFlow(float flow) {
+        if (flow < 0) flow = 0;
+        if (flow > 1) flow = 1;
+        this.flow = flow;
+    }
+
+    public void setSlope(float slope) {
+        if (slope < 0) slope = 0;
+        if (slope > 1) slope = 1;
+        this.slope = slope;
+    }
+
+    public void setLowestNeighbor(TileWithCoordinates lowestNeighbor) {
+        this.lowestNeighbor = lowestNeighbor;
+    }
+
+    public void addFlow(float flow) {
+        setFlow(getFlow() + flow);
+        }
 }
