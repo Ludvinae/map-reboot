4aa8d11564dba894884ced4ac5bbaf9f32e60ca3
Ludvinae <benoit.champlon79@gmail.com>
2026-02-02 18:25:14 +0100

    feat(world): added methods to compute lowest naighbor and slope in WorldQueries

--- src/com/yorkhuul/life/map/zone/world/WorldQueries.java
+++ src/com/yorkhuul/life/map/zone/world/WorldQueries.java
@@ -2,35 +2,61 @@ package com.yorkhuul.life.map.zone.world;
 
 import com.yorkhuul.life.map.zone.tile.Tile;
 import com.yorkhuul.life.map.zone.tile.TileConsumer;
+import com.yorkhuul.life.map.zone.tile.TileWithCoordinates;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class WorldQueries {
+public final class WorldQueries {
     // Lecture / calcul a partir du monde
 
-    private static final float SQRT2 = 1.41f;
+    private static final float SQRT2 = 1.41421356f;
 
-    public static TileConsumer getTileContext(World world, TileConsumer context) {
-
-        Tile neighbor = world.getLowestNeighbors(context);
+    /*
+    public static TileConsumer getTileContext(World world, int worldX, int worldY) {
+        Tile tile = world.getTileWithWorldCoordinates(worldX, worldY);
+        TileWithCoordinates neighbor = getLowestNeighbor(tile, getNeighbors(world, worldX, worldY));
         float distance; // 1 if orthogonal, 1.41 otherwise
-        float slope = WorldQueries.getSlope(tile, neighbor, distance);
-
+        if (neighbor.getWorldX() == worldX || neighbor.getWorldY() == worldY) distance = 1f;
+        else distance = SQRT2;
+        float slope = WorldQueries.getSlope(tile, neighbor.getTile(), distance);
 
     }
 
-    public static List<Tile> getNeighbor(World world, int worldX, int worldY){
-        List<Tile> neighbors = new ArrayList<>();
+     */
+
+    public static List<TileWithCoordinates> getNeighbors(World world, int worldX, int worldY){
+        List<TileWithCoordinates> neighbors = new ArrayList<>();
 
         for (int i = -1; i <= 1; i++) {
             for (int j = -1; j <= 1; j++) {
                 if (i == 0 && j == 0) continue;
 
-                Tile neighbor = world
-                neighbors.add(neighbor);
+                int x = worldX + j;
+                int y = worldY + i;
+
+                if (world.isInBounds(x, y)) {
+                    Tile neighbor = world.getTileWithWorldCoordinates(x, y);
+                    neighbors.add(new TileWithCoordinates(neighbor, x, y));
+                }
             }
         }
+        return neighbors;
+    }
+
+    public static TileWithCoordinates getLowestNeighbor(Tile tile, List<TileWithCoordinates> neighbors) {
+        float altitude = tile.getAltitude();
+        float minAltitude = altitude;
+        TileWithCoordinates lowestNeighbor = null;
+
+        for (TileWithCoordinates neighbor: neighbors) {
+            float neighborAltitude = neighbor.getAltitude();
+            if (neighborAltitude < minAltitude) {
+                minAltitude = neighborAltitude;
+                lowestNeighbor = neighbor;
+            }
+        }
+        return lowestNeighbor;
     }
 
 
