b7ad9a35dbf922aaaa3731f1e7e4c27279920d2b
unknown <benoit.champlon79@gmail.com>
2026-02-02 16:55:02 +0100

    feat(world): added incomplete methods in WorldQueries, WIP

--- src/com/yorkhuul/life/map/zone/world/WorldQueries.java
+++ src/com/yorkhuul/life/map/zone/world/WorldQueries.java
@@ -3,14 +3,41 @@ package com.yorkhuul.life.map.zone.world;
 import com.yorkhuul.life.map.zone.tile.Tile;
 import com.yorkhuul.life.map.zone.tile.TileContext;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class WorldQueries {
     // Lecture / calcul a partir du monde
 
+    private static final float SQRT2 = 1.41f;
+
     public static TileContext getTileContext(World world, TileContext context) {
 
-        Tile neighbor = world.getLowestNeighbors(context.);
-        float slope = WorldQueries.getSlope();
+        Tile neighbor = world.getLowestNeighbors(context);
+        float distance; // 1 if orthogonal, 1.41 otherwise
+        float slope = WorldQueries.getSlope(tile, neighbor, distance);
 
 
+    }
+
+    public static List<Tile> getNeighbor(World world, int worldX, int worldY){
+        List<Tile> neighbors = new ArrayList<>();
+
+        for (int i = -1; i <= 1; i++) {
+            for (int j = -1; j <= 1; j++) {
+                if (i == 0 && j == 0) continue;
+
+                Tile neighbor = world
+                neighbors.add(neighbor);
+            }
+        }
+    }
+
+
+    public static float getSlope(Tile tile, Tile neighbor, float distance) {
+        float altitude = tile.getAltitude();
+        float neighborAltitude = neighbor.getAltitude();
+
+        return (altitude - neighborAltitude) * distance;
     }
 }
