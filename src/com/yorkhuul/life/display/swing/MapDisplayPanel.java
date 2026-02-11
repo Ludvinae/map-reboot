3b7db25a5fb8b62c6fb3834ace5e59050b69feff
Ludvinae <benoit.champlon79@gmail.com>
2026-02-11 11:24:01 +0100

    fix(swing): added missing root panel in MapDisplayPanel.form

--- src/com/yorkhuul/life/display/swing/MapDisplayPanel.java
+++ src/com/yorkhuul/life/display/swing/MapDisplayPanel.java
@@ -7,6 +7,7 @@ import java.awt.image.BufferedImage;
 public class MapDisplayPanel extends JPanel {
 
     private BufferedImage image;
+    private JPanel mapPanel;
 
     public void setImage(BufferedImage image) {
         this.image = image;
