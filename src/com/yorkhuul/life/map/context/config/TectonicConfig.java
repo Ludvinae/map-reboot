2a5d636f69b85ec5f5b59d7db495b83cf4bcdaf8
Ludvinae <benoit.champlon79@gmail.com>
2026-02-11 09:43:59 +0100

    feat(context): implemented TectonicConfig with attributes, getters and setters

---
+++ src/com/yorkhuul/life/map/context/config/TectonicConfig.java
@@ -0,0 +1,77 @@
+package com.yorkhuul.life.map.context.config;
+
+public class TectonicConfig implements StepConfig{
+
+    private int count;
+    private String type;
+    private float frequency;
+    private float minRadius;
+    private float maxRadius;
+    private float distanceMin;
+    private float distanceMax;
+    private float strength;
+
+    public int getCount() {
+        return count;
+    }
+
+    public void setCount(int count) {
+        this.count = count;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public float getFrequency() {
+        return frequency;
+    }
+
+    public void setFrequency(float frequency) {
+        this.frequency = frequency;
+    }
+
+    public float getMinRadius() {
+        return minRadius;
+    }
+
+    public void setMinRadius(float minRadius) {
+        this.minRadius = minRadius;
+    }
+
+    public float getMaxRadius() {
+        return maxRadius;
+    }
+
+    public void setMaxRadius(float maxRadius) {
+        this.maxRadius = maxRadius;
+    }
+
+    public float getDistanceMin() {
+        return distanceMin;
+    }
+
+    public void setDistanceMin(float distanceMin) {
+        this.distanceMin = distanceMin;
+    }
+
+    public float getDistanceMax() {
+        return distanceMax;
+    }
+
+    public void setDistanceMax(float distanceMax) {
+        this.distanceMax = distanceMax;
+    }
+
+    public float getStrength() {
+        return strength;
+    }
+
+    public void setStrength(float strength) {
+        this.strength = strength;
+    }
+}
