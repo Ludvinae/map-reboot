b10c10de9b8a770ac3c79c879a52a6bb1dcee39f
Ludvinae <benoit.champlon79@gmail.com>
2026-02-13 10:32:50 +0100

    feat(generator): adapted Rain step to new contract

--- src/com/yorkhuul/life/map/steps/generator/hydrology/Rain.java
+++ src/com/yorkhuul/life/map/steps/generator/hydrology/Rain.java
@@ -1,5 +1,8 @@
 package com.yorkhuul.life.map.steps.generator.hydrology;
 
+import com.yorkhuul.life.map.config.hydrology.RainConfig;
+import com.yorkhuul.life.map.parameters.FloatParameter;
+import com.yorkhuul.life.map.parameters.IntParameter;
 import com.yorkhuul.life.map.parameters.Parameter;
 import com.yorkhuul.life.map.context.EditorContext;
 import com.yorkhuul.life.map.shape.effect.AddWaterTarget;
@@ -15,29 +18,16 @@ import com.yorkhuul.life.map.zone.world.World;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Rain implements HydrologyStep {
-
-    private int count;
-    private int minRadius;
-    private int maxRadius;
-    private float rainfallAmount;
-    List<Parameter<?>> parameters = new ArrayList<>();
-
-    public Rain(int count, int minRadius,int maxRadius, float rainfallAmount) {
-        this.count = count;
-        this.minRadius = minRadius;
-        this.maxRadius = maxRadius;
-        this.rainfallAmount = rainfallAmount;
-    }
+public class Rain implements HydrologyStep<RainConfig> {
 
     @Override
-    public void apply(World world, EditorContext context) {
+    public void apply(World world, RainConfig config) {
 
-        for (int i = 0; i < count; i++) {
+        for (int i = 0; i < config.getCount(); i++) {
             Coordinates center = new RandomRainSpot(world).getCoords();
-            int radius = new RandomInteger(this.minRadius, this.maxRadius).getRandomInt();
+            int radius = new RandomInteger(config.getMinRadius(), config.getMaxRadius()).getRandomInt();
 
-            float rainfall = (float) (Math.random() * rainfallAmount);
+            float rainfall = (float) (Math.random() * config.getRainfallAmount());
 
             Shape circle = new CircleShape(center, radius, rainfall);
             EffectTarget target = new AddWaterTarget();
@@ -54,7 +44,15 @@ public class Rain implements HydrologyStep {
     }
 
     @Override
-    public List<Parameter<?>> getParameters() {
+    public List<Parameter<?>> createParameters(RainConfig config) {
+        List<Parameter<?>> parameters = new ArrayList<>();
+
+        parameters.add(new IntParameter("Iterations count", 1, 100, config.getCount(), config::setCount));
+        parameters.add(new IntParameter("Minimum influence radius", 1, 100, config.getMinRadius(), config::setMinRadius));
+        parameters.add(new IntParameter("Maximum influence radius", 1, 100, config.getMaxRadius(), config::setMaxRadius));
+        parameters.add(new FloatParameter("Rainfall amount", 0.01f, 1f, config.getRainfallAmount(), 0.01f, config::setRainfallAmount));
+
         return parameters;
     }
+
 }
